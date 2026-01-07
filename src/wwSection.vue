d54b6d91eee68c7ed2cbf85ffdd350e3af411181
charlesmariga <charlesmariga37@gmail.com>
2026-01-07 11:38:37 +0300

    feat: update import

--- src/wwSection.vue
+++ src/wwSection.vue
@@ -4,7 +4,7 @@
 
 <script>
 import "../package/dist/orbiter-copilot.css";
-import { CopilotApp } from "../package/dist/orbiter-copilot.umd.cjs";
+import { CopilotApp } from "../package/dist/orbiter-copilot";
 
 export default {
   components: {
