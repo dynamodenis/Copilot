444288ea03c4d7af9b7bf63a4b29ac0fdb12fdf7
charlesmariga <charlesmariga37@gmail.com>
2026-01-07 14:22:24 +0300

    fix: thesysai provider

--- package/src/react_app/components/CopilotApp.tsx
+++ package/src/react_app/components/CopilotApp.tsx
@@ -1,5 +1,22 @@
-import { ThemeProvider } from "@thesysai/genui-sdk";
+import { C1Component, ThemeProvider } from "@thesysai/genui-sdk";
+import "@crayonai/react-ui/styles/index.css";
+import { useEffect, useState } from "react";
 
 export default function CopilotApp() {
-  return <ThemeProvider>CopilotApp</ThemeProvider>;
+  const [c1Response, setC1Response] = useState<string>("");
+
+  useEffect(() => {
+    const fetchResponse = async () => {
+      const response = await fetch("/chat");
+      const text = await response.text();
+      setC1Response(text);
+    };
+    fetchResponse();
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <C1Component c1Response={c1Response} isStreaming={false} />
+    </ThemeProvider>
+  );
 }
