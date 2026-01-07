24017db147cd5b14a084506358b1e752023995f8
charlesmariga <charlesmariga37@gmail.com>
2026-01-07 15:14:28 +0300

    test: removed provider

--- package/src/react_app/components/CopilotApp.tsx
+++ package/src/react_app/components/CopilotApp.tsx
@@ -1,15 +1,3 @@
-// import "./copilot.scss";
-// export default function CopilotApp() {
-//   return <h1 className="copilot-heading">Hello copilot</h1>;
-// }
-
-import { ThemeProvider } from "@thesysai/genui-sdk";
-import "@crayonai/react-ui/styles/index.css";
-
 export default function CopilotApp() {
-  return (
-    <ThemeProvider>
-      <h1>Hello copilot</h1>
-    </ThemeProvider>
-  );
+  return <h1>Hello copilot</h1>;
 }
